433ddd8d40740d34be8da9580bec0b48119c117f
Vincent Kamemia <kamemia18@yahoo.com>
2024-01-15 15:32:00 +0300

    Fixed calender issue

--- src/routes/transactions/+page.svelte
+++ src/routes/transactions/+page.svelte
@@ -1,10 +1,18 @@
 <script>
-    // import TransactionsCard from "$lib/components/Cards/TransactionsCard.svelte";
+    import TransactionsCard from "$lib/components/Cards/TransactionsCard.svelte";
     import { InlineCalendar } from 'svelte-calendar';
+    
+    const theme = {
+		calendar: {
+			width: '400px',
+			shadow: '0px 0px 5px rgba(0, 0, 0, 0.25)'
+		}
+	};
     
 </script>
 
-<!-- <div class="flex items-center">
+<div class="flex items-center">
     <div class="pt-24 pb-12"><TransactionsCard /></div>
-</div> -->
-<InlineCalendar />
+</div>
+
+<InlineCalendar {theme}/>
